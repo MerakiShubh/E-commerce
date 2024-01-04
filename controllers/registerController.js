c716a697cff900af478332718caef7cea7c35cef
MerakiShubh <shubhamt.champ@gmail.com>
2024-01-05 02:49:40 +0530

    registerController for userregisteration

---
+++ controllers/registerController.js
@@ -0,0 +1,26 @@
+import Joi from "joi";
+const registerController = {
+  register(req, res, next) {
+    //validate request
+
+    const registerSchema = Joi.object({
+      name: Joi.string().min(3).max(30).required(),
+      email: Joi.string().email().required(),
+      password: Joi.string()
+        .pattern(new RegExp("^[a-zA-Z0-9]{3,30}$"))
+        .required(),
+      repeat_password: Joi.ref("password"),
+    });
+
+    const { error } = registerSchema.validate(req.body);
+
+    if (error) {
+      //For errorhandling passing error to a middleware in errorHandling.js file
+      //middlware can't catch all type of error specially async function error we have to do it like this
+      return next(error);
+    }
+    res.json({ message: "Hello from express" });
+  },
+};
+
+export default registerController;
