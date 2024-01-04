6eafed4edd2ff3d2deb7c5851405432a19315590
MerakiShubh <shubhamt.champ@gmail.com>
2024-01-05 02:47:28 +0530

    Middleware: errorHandler and express.json registered

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 import express from "express";
 import { APP_PORT } from "./config";
 const app = express();
+import routes from "./routes";
+app.use(express.json());
+import errorHandler from "./middlewares/errorHandler";
+app.use("/api", routes);
+//Dyan rahe middleware register hogi app.listen ke upar
+
+app.use(errorHandler);
 
 app.listen(APP_PORT, () => console.log(`Listening on port ${APP_PORT}`));
