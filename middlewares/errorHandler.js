0e78f4875551ce978ac9b84366b373d5b9cd0017
MerakiShubh <shubhamt.champ@gmail.com>
2024-01-05 17:28:22 +0530

    error handled if email already exists

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,5 +1,6 @@
 import { DEBUG_MODE } from "../config";
 import { ValidationError } from "joi";
+import CustomeErrorHandler from "../services/CustomeErrorHandler";
 const errorHandler = (err, req, res, next) => {
   let statusCode = 500;
   let data = {
@@ -12,6 +13,13 @@ const errorHandler = (err, req, res, next) => {
 
   if (err instanceof ValidationError) {
     statusCode = 422;
+    data = {
+      messgae: err.message,
+    };
+  }
+
+  if (err instanceof CustomeErrorHandler) {
+    statusCode = err.status;
     data = {
       messgae: err.message,
     };
