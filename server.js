212d5c27ae36c18d98d60fbba0d01cfc8b9e885d
MerakiShubh <shubhamt.champ@gmail.com>
2024-01-06 23:30:45 +0530

    Mongoose connection established

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 import express from "express";
 import { APP_PORT } from "./config";
-const app = express();
-import routes from "./routes";
-app.use(express.json());
 import errorHandler from "./middlewares/errorHandler";
+const app = express();
+
+
+mongoose.connect(DB_URL, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
+const db = mongoose.connection;
+db.on('error', console.error().bind(console, 'connection error:'))
+db.once('open', ()=>{
+    console.log("DB connecte...")
+})
+
+import routes from "./routes";
+import mongoose from "mongoose";
+app.use(express.json());
 app.use("/api", routes);
 //Dyan rahe middleware register hogi app.listen ke upar
 
